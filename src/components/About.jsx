944e9d565ea19d7b2093207030ed410f5839734a
bontlenkoale1 <bontlenkoale20@gmail.com>
2025-05-20 20:25:43 +0200

    added about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,25 +1,35 @@
-import React from 'react';
-import './About.css';
+import React from "react";
+import "./About.css";
 
 const About = () => {
-  return (
-    <div className="about-wrapper">
-      <div className="about-image-content">
-        <img src="public/Images/image.png" alt="about-image" />
-    </div>
-    <div className="about-text-content-right">
-        <h1>About Us <span className="hover-underline"></span></h1>
-        <div className="about-text-content">
-        <p>The Apartheid Museum opened in 2001 and is acknowledged as</p>
-        <p>the pre-eminent museum in the world dealing with 20th century</p><p>South Africa, at the heart of which is the apartheid story.<bold><br />The Apartheid Museum,the first of its kind, illustrates the rise</bold></p>
-        <p><bold> and fall of apartheid.<br />An architectural consortium, comprising</bold></p>
-        <p><bold>several leading architectural firms, conceptualised the design of the building </bold></p>
-        <p><bold>on a seven-hectare stand. The museum is a superb example of design,space and landscape</bold></p>
-        <p><bold> offering a unique experience to local and international visitors.</bold></p>
+    return (
+        <div className="about-wrapper">
+            <div className="about-image-content">
+                <img src="public/Images/image.png" alt="about-image" />
+            </div>
+            <div className="about-text-content-right">
+                <h1>
+                    About Us <span className="hover-underline"></span>
+                </h1>
+                <div className="about-text-content">
+                    <p>
+                        The Apartheid Museum opened in 2001 and is acknowledged as the pre-eminent museum in the world
+                        dealing with 20th century South Africa, at the heart of which is the apartheid story.
+                    </p>
+                    <p>
+                        <bold>
+                            The Apartheid Museum,the first of its kind, illustrates the rise and fall of apartheid.An
+                            architectural consortium, comprising several leading architectural firms, conceptualised the
+                            design of the building on a seven-hectare stand. The museum is a superb example of
+                            design,space and landscape offering a unique experience to local and international visitors</bold>
+                    </p>
+                </div>
+                <div className="about-button-wrapper">
+                  <button className="read-more-button">Read More</button>
+                </div>
+            </div>
         </div>
-    </div>
-    </div>
-  );
+    );
 };
 
 export default About;
