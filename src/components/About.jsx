f17e827eee186ac45e825d7e8e9ff799e3f4fde1
bontlenkoale1 <bontlenkoale20@gmail.com>
2025-05-21 23:18:34 +0200

    added important note

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -21,11 +21,12 @@ const About = () => {
                             The Apartheid Museum,the first of its kind, illustrates the rise and fall of apartheid.An
                             architectural consortium, comprising several leading architectural firms, conceptualised the
                             design of the building on a seven-hectare stand. The museum is a superb example of
-                            design,space and landscape offering a unique experience to local and international visitors</bold>
+                            design,space and landscape offering a unique experience to local and international visitors
+                        </bold>
                     </p>
                 </div>
                 <div className="about-button-wrapper">
-                  <button className="read-more-button">Read More</button>
+                    <button className="read-more-button">Read More</button>
                 </div>
             </div>
         </div>
