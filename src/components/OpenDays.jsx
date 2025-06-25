c0b590c4d45dde3e15ca825b0dcb66dcb04262fd
Bontle <126960560+bontlenkoale1@users.noreply.github.com>
2025-06-25 14:22:47 +0200

    Revert "added banner media queries"

--- src/components/OpenDays.jsx
+++ src/components/OpenDays.jsx
@@ -14,20 +14,20 @@ const OpenDays = () => {
           />
         </div>
       </div>
-     <div className="open-days-container">
-  <div className="open-days-wrapper">
-    <div className="clock-icon-container">
-      <svg className="clock-icon" width="44" height="44" viewBox="0 0 44 44" fill="none" xmlns="http://www.w3.org/2000/svg">
+     <div class="open-days-container">
+  <div class="open-days-wrapper">
+    <div class="clock-icon-container">
+      <svg class="clock-icon" width="44" height="44" viewBox="0 0 44 44" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path
           d="M22 10V22L30 26M42 22C42 33.0457 33.0457 42 22 42C10.9543 42 2 33.0457 2 22C2 10.9543 10.9543 2 22 2C33.0457 2 42 10.9543 42 22Z"
           stroke="#F3F3F3"
-          strokeWidth="4"
-          strokeLinecap="round"
-          strokeLinejoin="round"
+          stroke-width="4"
+          stroke-linecap="round"
+          stroke-linejoin="round"
         />
       </svg>
     </div>
-    <div className="open-days-text">
+    <div class="open-days-text">
       <p>We are open from Wednesday to Sundays, 09:00 to 17:00 and most public holidays.</p>
       <p>Please note: We are closed on Good Friday and open on Easter Monday.</p>
       <p>Free entry for all South Africans on Freedom Day - 27 April 2025.</p>
