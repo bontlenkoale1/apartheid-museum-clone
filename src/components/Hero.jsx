26648380e5eaeb71cd9c6fd0e87372b153a569d3
bontlenkoale1 <bontlenkoale20@gmail.com>
2025-06-26 17:13:22 +0200

    updated media queries

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,32 +4,27 @@ import "./Hero.css";
 
 const Hero = () => {
   return (
-    <div>
-        <div className="container">
-            <div className="hero-wrapper">
-                <div className="hero-image">
-                </div>
-                <div className="hero-search-container">
-            <form className="hero-form">
-      <div className="search-input-wrapper">
-      <input
-        type="text"
-        id="search"
-        name="search"
-        placeholder="Search prompt..."
-        className="hero-input"
-      />
-      <button type="submit" className="search-button">
-        <FaSearch className="search-icon" />
-      </button>
-    </div>
-  </form>
-</div>
-
-          </div>
+    <section className="hero">
+      <div className="hero-background">
+        <div className="hero-content">
+          <form className="hero-form">
+            <div className="search-input-wrapper">
+              <input
+                type="text"
+                id="search"
+                name="search"
+                placeholder="Search prompt..."
+                className="hero-input"
+              />
+              <button type="submit" className="search-button">
+                <FaSearch className="search-icon" />
+              </button>
+            </div>
+          </form>
         </div>
-    </div>
+      </div>
+    </section>
   )
 }
 
-export default Hero
+export default Hero;
