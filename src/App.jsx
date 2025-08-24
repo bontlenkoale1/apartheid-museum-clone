ccf6399ea5ee8e8fd2bee8bd3d0b890052200864
bontlenkoale1 <bontlenkoale20@gmail.com>
2025-08-24 18:36:42 +0200

    added footer section without media queries

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,29 @@
 import React from "react";
-import {Navbar} from "./components/Navbar";
+import { Navbar } from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import OpenDays from "./components/OpenDays";
 import Exhibitions from "./components/Exhibitions";
 import PermanentExhibitions from "./components/PermanentExhibitions";
 import NewsLetter from "./components/NewsLetter";
+import Footer from "./components/Footer.jsx";
 import "./App.css";
 
 function App() {
-    return (
-        <div className="App">
-            <Navbar />
-            <Hero />
-            <About />
-            <OpenDays />
-            <Exhibitions />
-            <PermanentExhibitions />
-            <NewsLetter />
-        </div>
-    );
+  return (
+    <div className="App-container">
+      <Navbar />
+      <main>
+        <Hero />
+        <About />
+        <OpenDays />
+        <Exhibitions />
+        <PermanentExhibitions />
+        <NewsLetter />
+      </main>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,136 @@
+import React from "react";
+import "./Footer.css";
+import logo from "/Icons/logo.png";
+import { FaFacebook, FaTwitter, FaInstagram } from "react-icons/fa";
+
+const Footer = () => {
+  return  (
+    <footer className="footer-container">
+      <div className="footer-content">
+        <div className="footer-col">
+          <div className="footer-logo">
+            <img
+              src={logo}
+              alt="Apartheid Museum Logo"
+              className="logo-image"
+            />
+            <h2>Apartheid Museum</h2>
+          </div>
+        </div>
+        <div className="footer-col">
+          <div div className="footer-links">
+            <div className="footer-heading-row">
+              <h3>About </h3>
+            </div>
+            <ul>
+              <li>
+                <a href="#about-us">About Us</a>
+              </li>
+              <li>
+                <a href="#about-us-overview">About Us Overview</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="footer-col">
+          <div div className="footer-links">
+            <div className="footer-heading-row">
+              <h3>Exhibitions</h3>
+            </div>
+            <ul>
+              <li>
+                <a href="#permanent-exhibitions">Permanent Exhibitions</a>
+              </li>
+              <li>
+                <a href="#Mandela-exhibitions">Mandela Exhibition</a>
+              </li>
+              <li>
+                <a href="#hugh-masekela-exhibition">Hugh Masekela Exhibition</a>
+              </li>
+              <li>
+                <a href="#desmond-tutu-exhibition">Desmond Tutu Exhibition</a>
+              </li>
+              <li>
+                <a href="#past-exhibition">Past Exhibitions</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="footer-col">
+          <div div className="footer-links">
+            <div className="footer-heading-row">
+              <h3>School Visits</h3>
+            </div>
+            <ul>
+              <li>
+                <a href="#get-tickets">Get Tickets</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="footer-col">
+          <div div className="footer-links">
+            <div className="footer-heading-row">
+              <h3>Events</h3>
+            </div>
+            <ul>
+              <li>
+                <a href="#get-tickets">Get Tickets</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="footer-col">
+          <div div className="footer-links">
+            <div className="footer-heading-row">
+              <h3>Resources</h3>
+            </div>
+          </div>
+        </div>
+        <div className="footer-col">
+          <div div className="footer-links">
+            <div className="footer-heading-row">
+              <h3>Contact Us</h3>
+            </div>
+            <ul>
+              <li>
+                <a href="#contact-us">Contact Us</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="footer-bottom">
+          <p>
+            &copy; {new Date().getFullYear()} Apartheid Museum. All rights
+            reserved.
+          </p>
+          <div className="footer-social-icons">
+            <a
+              href="https://www.facebook.com/ApartheidMuseumSA?mibextid=ZbWKwL"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+               <FaFacebook style={{ color: "#fff", fontSize: "20px" }} />
+            </a>
+            <a
+              href="https://x.com/apartheidmuseum"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaTwitter style={{ color: "#fff", fontSize: "20px" }} />
+            </a>
+            <a
+              href="https://www.instagram.com/apartheidmuseum/?hl=en"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaInstagram style={{ color: "#fff", fontSize: "20px" }} />
+            </a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
